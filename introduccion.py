8cb5b8c67c1a7bf0a0bc1e8b21d6a5b70a30957d
JBE777 <49010974+JBE777@users.noreply.github.com>
2024-01-23 07:44:00 -0700

    Recomendacion

--- introduccion.py
+++ introduccion.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Sistema de Recomendacion")
 st.markdown("***")
 st.markdown("### Restaurantes Mexicanos en USA")
-st.sidebar.markdown("Usando sistemas de recomendacion")
+st.sidebar.markdown("Grupo 1: Marta-Hernan-Carlos-Steven-Javier")
