a51def3c275731d7d61d523f25ea80f147e32fd9
JBE777 <49010974+JBE777@users.noreply.github.com>
2024-01-24 11:09:49 -0700

    Recomendacion

--- introduccion.py
+++ introduccion.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("Introduccion")
 st.markdown("#### Con el proposito de generar mas diversidad de resultados en las consultas. Se construyeron los siguientes sistemas de recomendacion:")
+
 st.markdown("#### 1. Los top 5 Restaurantes Mexicanos en USA Estandar")
 st.markdown("Lista generada sin filtros")
 st.markdown("#### 2. Los top 5 Restaurantes Mexicanos en USA Mejor Calificados")
@@ -11,6 +12,7 @@ st.markdown("Lista generada filtrando por 4 estrellas")
 st.markdown("#### 4. Los top 5 Restaurantes Mexicanos en USA Peor Calificados")
 st.markdown("Lista generada filtrando por 1 estrella")
 st.markdown("***")
+
 st.sidebar.markdown("Part-Time 04 Data Science ")
 st.sidebar.markdown("Grupo 1: Marta-Hernan-Carlos-Steven-Javier")
 
